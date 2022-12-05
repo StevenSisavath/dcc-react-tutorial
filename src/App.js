2f3db07bd8dd46bd9a6711cab84f8f947e309ed2
Steven Sisavath <stevenlsisavath@gmail.com>
2022-12-05 08:04:34 -0600

    Complete AddEntryForm Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries'
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
 
 function App() {
 
   const [entries, setEntries] = useState([{weight: 175, date: '11-3-2021'}])
 
+  function addNewEntry(entry){
+    let tempEntries = [...entries, entry];
+    setEntries(tempEntries);
+  }
+
   return (
     <div className="App">
       <DisplayEntries parentEntries={entries} />
+      <AddEntryForm addNewEntryProperty={addNewEntry}/>
     </div>
   );
 }
